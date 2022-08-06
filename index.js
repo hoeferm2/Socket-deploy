4dc634fb2d663fe041fb07a80b268500fa9c19b2
Hoefer <ningboninja@gmail.com>
2022-08-06 11:47:41 -0700

    working

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+const FORM = '/form.js'
+
+
+app.get('/', (req, res) => {
+    res.sendFile(INDEX, { root: __dirname + '/public' });
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(FORM, { root: __dirname + '/public' });
+});
+
+
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+    });
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+});
+
+server.listen(PORT, () => console.log(`Listening on ${PORT}`));
